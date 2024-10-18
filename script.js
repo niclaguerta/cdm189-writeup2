e2baae643871097259b08416607350706e4afd83
Nicole Laguerta <coco@Nicoles-iMac.local>
2024-10-18 14:28:39 -0700

    first iteration

--- script.js
+++ script.js
@@ -10,22 +10,7 @@
         const dataPoints = Object.keys(data);
         globalData = Object.values(data);
         numDataPoints = dataPoints.length;
-    }
-
-    function moodInfo(point, data) {
-        const ke$haMoods = [
-            '<img src="images/chonk-1.png">',  // Mood 1
-            '<img src="images/chonk-2.png">',  // Mood 2
-            '<img src="images/chonk-3.png">'   // Mood 3
-        ];
-    
-        // Adjust to match mood to array
-        const moodIndex = data[point].mood - 1;
-    
-        document.querySelector('#activity').innerHTML = data[point].activity;
-        document.querySelector('#moods').innerHTML = ke$haMoods[moodIndex];
-        document.querySelector('#time').innerHTML = data[point].time;
-    }    
+    } 
 
     document.addEventListener('mousemove', reportPos);
 
